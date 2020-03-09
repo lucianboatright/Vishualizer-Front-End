af9a54e680e2acf218bfe9c2bbf6e796582807c6
Lucian Boatright <lucian.boatright@gmail.com>
2020-03-09 11:06:57 +0000

    push state

--- src/components/VisualizerInfo.js
+++ src/components/VisualizerInfo.js
@@ -1,53 +1,53 @@
-// import React, { Component } from 'react';
-// import './App.css';
+import React, { Component } from 'react';
+import './App.css';
 
-// class VisualizerInfo extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//     }
-//   }
-//   onTrackUpdated(track) {
-//     const spotifyURL = "https://api.spotify.com/v1/audio-features/"
-//     fetch(spotifyURL + track, {
-//       headers: {
-//         "Accept": "application/json",
-//         "Content-Type": "application/json",
-//         "Authorization": "Bearer " + this.props.oAuth
-//       }
-//     })
-//     .then(response => response.json())
-//     .then(response => {
-//       this.setState({
-//         track: track,
-//         visInfo: response,
-//       })
-//       console.log("SONG VIS RESPONCE INFOOO", response)
-//       sessionStorage.setItem("vis_info", response)
+class VisualizerInfo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  onTrackUpdated(track) {
+    const spotifyURL = "https://api.spotify.com/v1/audio-features/"
+    fetch(spotifyURL + track, {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + this.props.oAuth
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        track: track,
+        visInfo: response,
+      })
+      console.log("SONG VIS RESPONCE INFOOO", response)
+      sessionStorage.setItem("vis_info", response)
       
-//     })
-//     // console.log("VIS INFO ", console.log(JSON.stringify(sessionStorage.getItem("vis_info")))) 
-//     // console.log("SONG FEATURES INFOOO", sessionStorage.getItem('vis_info'))
-//   }
+    })
+    // console.log("VIS INFO ", console.log(JSON.stringify(sessionStorage.getItem("vis_info")))) 
+    // console.log("SONG FEATURES INFOOO", sessionStorage.getItem('vis_info'))
+  }
 
-//   componentDidMount() {
-//     this.onTrackUpdated(this.props.track);
-//   }
+  componentDidMount() {
+    this.onTrackUpdated(this.props.track);
+  }
 
-//   render() {
+  render() {
 
     
-//     if (!this.state.visInfo) {
-//       return <div></div>
-//     }
-//     return (
-//     <div className="VisualizerInfo">
-//      <div> OTHER INFO:  </div>
-//     </div>
-//     )
-//   }
-// }
+    if (!this.state.visInfo) {
+      return <div></div>
+    }
+    return (
+    <div className="VisualizerInfo">
+     <div> OTHER INFO:  </div>
+    </div>
+    )
+  }
+}
 
 
 
-// export default VisualizerInfo;
+export default VisualizerInfo;
