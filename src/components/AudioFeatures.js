4c2a34ddb4bfb4ceded5d8e6ad36c94b9e974658
Vish Mayer <vish.mayer@gmail.com>
2020-03-09 16:53:07 +0000

    Change data output - Co-Authored-by: lucianboatright <lucian.boatright@gmail.com>

--- src/components/AudioFeatures.js
+++ src/components/AudioFeatures.js
@@ -19,11 +19,8 @@ class AudioFeatures extends Component {
     .then(response => {
       this.setState({
         track: track,
-        songFeatures: response, 
+        songFeatures: response,
       })
-      console.log("SONG FEATURE RESPOSNCE", response)
-      localStorage.setItem("sound_features", response)
-      
     })
   }
 
@@ -34,28 +31,56 @@ class AudioFeatures extends Component {
   render() {
 
     const keyInteger = {0: 'C', 1: 'C♯/D♭', 2: 'D', 3: 'D♯/E♭', 4: 'E', 5: 'F', 6: 'F♯/G♭', 7: 'G', 8: 'G♯/A♭', 9: 'A', 10: 'A♯/B♭', 11: 'B'}
+    const getDancelevel = {0: 'Low', 0.1: 'Low', 0.2: 'Low', 0.3: 'Low', 0.4: 'Medium', 0.5: 'Medium', 0.6: 'Medium', 0.7: 'High', 0.8: 'High', 0.9: 'High', 1.0: 'High'}
+    const getEnergylevel = {0: 'Low', 0.1: 'Low', 0.2: 'Low', 0.3: 'Low', 0.4: 'Medium', 0.5: 'Medium', 0.6: 'Medium', 0.7: 'High', 0.8: 'High', 0.9: 'High', 1.0: 'High'}
+    const getValencelevel = {0: 'Melancholy', 0.1: 'Melancholy', 0.2: 'Melancholy', 0.3: 'Melancholy', 0.4: 'Neutral', 0.5: 'Neutral', 0.6: 'Neutral', 0.7: 'Positive', 0.8: 'Positive', 0.9: 'Positive', 1.0: 'Positive'}
+    const getLivenessLevel = {0: 'Studio', 0.1: 'Studio', 0.2: 'Studio', 0.3: 'Studio', 0.4: 'Live Recording', 0.5: 'Live Recording', 0.6: 'Live Recording', 0.7: 'Live Recording', 0.8: 'Live Recording', 0.9: 'Live Recording', 1.0: 'Live Recording'}
+
+    function setLoudnessLevel(getLoudnesStat) {
+      if (getLoudnesStat < -35 ){
+        return "Quiet"
+      } else if (getLoudnesStat > -35 && getLoudnesStat < -15) {
+        return "Moderate"
+      } else {
+        return "Loud"
+      }
+    }
+
+
+    function setInstrumentalnessValue(getInstrumentalness) {
+      if (getInstrumentalness > .6) {
+        return "No Vocals"
+      } else {
+        return "Has Vocals"
+      }
+    }
+
+    function millisToMinutesAndSeconds(getTimeDuration) {
+      var minutes = Math.floor(getTimeDuration / 60000);
+      var seconds = ((getTimeDuration % 60000) / 1000).toFixed(0);
+      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
     if (!this.state.songFeatures) {
       return <div></div>
     }
+    
+    
     return (
     <div className="AudioFeatures">
-        <div> Selected song danceability: { this.state.songFeatures.danceability }</div>
-
+        <div> Selected song danceability: { getDancelevel[parseFloat(this.state.songFeatures.danceability).toFixed(1)] } Danceability</div>
         <div> Selected song key: { keyInteger[this.state.songFeatures.key] }</div>
         <div> Selected song time signature: { this.state.songFeatures.time_signature }</div>
-        <div> Selected song duration: { this.state.songFeatures.duration_ms }</div>
-        <div> Selected song energy: { this.state.songFeatures.energy }</div>
-        <div> Selected song tempo: { this.state.songFeatures.tempo }</div>
+        <div> Selected song duration: { millisToMinutesAndSeconds(this.state.songFeatures.duration_ms) }</div>
+        <div> Selected song energy: { getEnergylevel[parseFloat(this.state.songFeatures.energy).toFixed(1)] } Energy</div>
+        <div> Selected song tempo: { parseFloat(this.state.songFeatures.tempo).toFixed(0) } BPM</div>
         <div> Selected song acousticness: { this.state.songFeatures.acousticness }</div>
-        <div> Selected song instrumentalness: { this.state.songFeatures.instrumentalness }</div>
-        <div> Selected song valence: { this.state.songFeatures.valence }</div>
-        <div> Selected song liveness: { this.state.songFeatures.liveness }</div>
-        <div> Selected song loudness: { this.state.songFeatures.loudness }</div>
+        <div> Selected song instrumentalness: { setInstrumentalnessValue(this.state.songFeatures.instrumentalness) }</div>
+        <div> Selected song valence: { getValencelevel[parseFloat(this.state.songFeatures.valence).toFixed(1)] }</div>
+        <div> Selected song liveness: { getLivenessLevel[parseFloat(this.state.songFeatures.liveness).toFixed(1)] }</div>
+        <div> Selected song loudness: { setLoudnessLevel(this.state.songFeatures.loudness) }</div>
     </div>
     )
   }
 }
-console.log("END AUDIO FEATURE CLASS", localStorage.getItem('sound_features'))
-
 
 export default AudioFeatures;
