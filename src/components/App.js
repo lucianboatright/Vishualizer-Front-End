3888b599e7e8c0a8a3e3e1b802c521c5117010e5
Vish Mayer <vish.mayer@gmail.com>
2020-03-11 14:36:48 +0000

    regroup of song info layout

--- src/components/App.js
+++ src/components/App.js
@@ -149,7 +149,7 @@ class App extends Component {
               <div className="currently-playing">
                 <div className="now-playing">
 
-                  <h2>Now Playing:</h2>
+                  
                   <h5>
                     {this.state.nowPlaying.name} 
                     <h4>
